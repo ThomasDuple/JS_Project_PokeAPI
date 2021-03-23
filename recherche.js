a326d7278ed74a7d7e9e32ab342cedbd517f879b
Clément Guiton <clement.guiton.dev@gmail.com>
2021-03-23 17:20:47 +0100

    affichage h2

--- recherche.js
+++ recherche.js
@@ -1,6 +1,19 @@
 function recherche(){
-    let categorie = document.getElementById("categorie").value;
-    let input = document.getElementById("textInput").value;
-    let search = categorie + "/" + input;
-    alert(search);
+    let input = $( "#textInput" ).val();
+    $.ajax({url: "https://pokeapi.co/api/v2/pokemon/"+input, success: function(result){
+        //Abilities
+        console.log(result);
+        //result.abilities.forEach(element => $("#abilities").append(createP(element.ability.name)));
+        for(const i in result){
+          let element = document.createElement("h2");
+          element.innerHTML = i;
+          console.log(element)
+          $("#bloc-resultats").append(element);
+        }
+      }});
 }
+
+function createP(text, class_name = ""){
+  return "<p class=\"" + class_name + "\">" + text + "</p>"
+}
+
