c96d5a2ef8d5392997540216e04bda9481327e1f
Clément Guiton <clement.guiton.dev@gmail.com>
2021-03-23 16:19:27 +0100

    ajout jquery

--- recherche.js
+++ recherche.js
@@ -1,3 +1,6 @@
 function recherche(){
-    alert("recherche")
+    let categorie = document.getElementById("categorie").value;
+    let input = document.getElementById("textInput").value;
+    let search = categorie + "/" + input;
+    alert(search);
 }
