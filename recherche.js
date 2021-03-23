298e52136b6deebde5e08f821c919fd68fc5afba
ThomasDuple <thomasduple@gmail.com>
2021-03-23 16:34:49 +0100

    vite fait

--- recherche.js
+++ recherche.js
@@ -1,3 +1,18 @@
 function recherche(){
     alert("recherche")
 }
+
+function addToFavorite() {
+    
+
+    const fav = localStorage.getItem("favorites");
+    fav.push()
+    localStorage.setItem("favorites", fav);
+}
+
+function removeFromFavorite(item) {
+    const fav = localStorage.getItem("favorites");
+    const i = fav.indexOf(item);
+    fav.splice(i, 1);
+    localStorage.setItem("favorites", fav);
+}
